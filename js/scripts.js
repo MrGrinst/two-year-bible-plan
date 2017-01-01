bfa668bbc293bcf328fd18a229610e0753149e47
Kyle Grinstead <kyle@kylegrinstead.com>
2017-01-01 01:36:34 -0500

    Made progress in the UI.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,32 @@
-function openESVBibleTabsForDate(date) {
-    var dayData = data[daysFromPlanStartTo(date)];
-    for (var key in dayData) {
-        if (key !== "Date") {
-            var references = dayData[key].split(",");
-            for (var referenceIndex in references) {
-                openESVBibleTabForReference(references[referenceIndex].trim());
-            }
-        }
-    }
+function openESVBibleTabsForToday(date) {
+    var planData = Papa.parse(biblePlanCSV, { header: true });
+    var dayData = planData.data[daysFromPlanStartTo(date)];
+    $("#old_testament_button").html(dayData["Old Testament"]);
+    $("#new_testament_button").html(dayData["New Testament"]);
+    $("#psalm_button").html(dayData["Psalm"]);
+    $("#proverbs_button").html(dayData["Proverbs"]);
 }
 
 function daysFromPlanStartTo(date) {
-    var oneDay = 24 * 60 * 60 * 1000;
-    var dateAdjustedForLateBedtime = new Date(date.getTime() - 4 * 60 * 60 * 1000);
-    var startingDay = new Date(2017, 0, 1);
-    return Math.floor(Math.abs((startingDay.getTime() - dateAdjustedForLateBedtime.getTime())/(oneDay)));
+    var startingDay = moment([2017, 0, 1]);
+    return Math.floor(startingDay.diff(currentDayAdjustedForLateBedtime(), 'day'));
+}
+
+function currentDayAdjustedForLateBedtime() {
+    return moment().subtract(extraTimeToAccountForLateBedtimes(), 'hours');
+}
+
+function currentDayFormatted() {
+    return moment().format('MMMM D, YYYY');
+}
+
+function isAfterMidnightButBeforeBedtime() {
+    return moment().hour() < extraTimeToAccountForLateBedtimes();
+}
+
+function extraTimeToAccountForLateBedtimes() {
+    var queryBedtime = getParameterByName("bedtime");
+    return parseInt(queryBedtime) || 4;
 }
 
 function openESVBibleTabForReference(reference) {
@@ -24,5 +36,23 @@ function openESVBibleTabForReference(reference) {
     document.body.appendChild(iframe);
 }
 
-var currentDay = new Date();
-openESVBibleTabsForDate(currentDay);
+function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+$(function() {
+    openESVBibleTabsForToday();
+    $("#date").html(currentDayFormatted());
+    if (isAfterMidnightButBeforeBedtime()) {
+        $("#date").append("*");
+        $("#date_has_been_adjusted_explanation").show();
+    }
+});
